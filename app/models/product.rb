4e4bcc6ed25ce1a6f8c95d1e4cb73ef41b8de7cf
sarahbaranski <sbaranski@gmail.com>
2021-01-18 21:22:55 -0600

    added happy and sad paths

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { minimum: 2, maximum: 500 }
+  #validates :image_url, :with => %r{\.(png|jpg|jpeg)$}i,
   def is_discounted
     price <= 300
   end
--- db/migrate/20210119022459_add_inventoryto_products.rb
+++
@@ -1,5 +0,0 @@
-class AddInventorytoProducts < ActiveRecord::Migration[6.0]
-  def change
-    add_colum :products, :inventory, :integer
-  end
-end
