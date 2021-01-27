cf050036082bd974af8e975eedd916732e3ba39e
sarahbaranski <sbaranski@gmail.com>
2021-01-26 21:29:32 -0600

    created category, category_product controllers and added join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :categories, through: :category_products
 
   def is_discounted
     price <= 300
