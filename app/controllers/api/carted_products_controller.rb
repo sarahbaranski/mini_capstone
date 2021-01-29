c555852afb6827a06780be1c319f7047e42334aa
sarahbaranski <sbaranski@gmail.com>
2021-01-28 21:20:36 -0600

    added index and create method in cp controller with index and show

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,19 +1,21 @@
 class Api::CartedProductsController < ApplicationController
   def index
-    @cartedproducts = current_user.cartedproducts
+    @carted_products = current_user.carted_products.where(status: "Carted")
     render "index.json.jb"
   end
 
   def create
-    @cartedproducts = CartedProduct.new({
+    @carted_product = CartedProduct.new({
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       status: "Carted",
       order_id: " ",
     })
-    if @cartedproducts.save
+    if @carted_product.save
       render "show.json.jb"
-    end #add and else statement here
+    else
+      render json: { errors: @carted_product.errors.full_messages }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     post "/orders" => "orders#create"
     get "/orders/:id" => "orders#show"
 
-    get "cartedproducts" => "cartedproducts#index"
-    post "/cartedproducts" => "cartedproducts#create"
+    get "/carted_products" => "carted_products#index"
+    post "/carted_products" => "carted_products#create"
   end
 end
