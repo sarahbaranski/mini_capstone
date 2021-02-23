687f78f27b52693c21ae227c63586b7e0200cc95
sarahbaranski <sbaranski@gmail.com>
2021-02-22 20:36:54 -0600

    configure cors for netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "mini-capstone.netlify.app"
+    origins "localhost:8080", "mini-capstone.netlify.app", "quizzical-einstein-c05147.netlify.app"
 
     resource "*",
       headers: :any,
